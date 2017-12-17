b20abc118cb15b846d18d0a70fe5e799b68eff1c
Julia Janicki <jhjanicki@gmail.com>
2017-12-17 14:17:24 +0900

    first

---
+++ data/data.js
@@ -0,0 +1,233 @@
+var data = 
+[
+  {
+    "name": "Shihuahuaco",
+    "value": 1067,
+    "china": 772
+  },
+  {
+    "name": "Cachimbo",
+    "value": 283,
+    "china": 1
+  },
+  {
+    "name": "Estoraque",
+    "value": 204,
+    "china": 150
+  },
+  {
+    "name": "Cumala",
+    "value": 154,
+    "china": 0
+  },
+  {
+    "name": "Ishpingo",
+    "value": 108,
+    "china": 3
+  },
+  {
+    "name": "Huayruro",
+    "value": 108,
+    "china": 1
+  },
+  {
+    "name": "Tornillo",
+    "value": 61,
+    "china": 4
+  },
+  {
+    "name": "Congona",
+    "value": 54,
+    "china": 0
+  },
+  {
+    "name": "Capirona",
+    "value": 37,
+    "china": 5
+  },
+  {
+    "name": "Tahuari",
+    "value": 33,
+    "china": 14
+  },
+  {
+    "name": "Marupa",
+    "value": 33,
+    "china": 1
+  },
+  {
+    "name": "Quinilla",
+    "value": 28,
+    "china": 4
+  },
+  {
+    "name": "Azucar huayo",
+    "value": 22,
+    "china": 15
+  },
+  {
+    "name": "Protium sp.",
+    "value": 19,
+    "china": 0
+  },
+  {
+    "name": "Nogal",
+    "value": 15,
+    "china": 6
+  },
+  {
+    "name": "Ana Caspi",
+    "value": 14,
+    "china": 2
+  },
+  {
+    "name": "Cedro",
+    "value": 14,
+    "china": 0
+  },
+  {
+    "name": "Carapa guianensis",
+    "value": 12,
+    "china": 0
+  },
+  {
+    "name": "Leche caspi",
+    "value": 12,
+    "china": 0
+  },
+  {
+    "name": "Andiroba",
+    "value": 11,
+    "china": 0
+  },
+  {
+    "name": "Copaiba",
+    "value": 7,
+    "china": 4
+  },
+  {
+    "name": "Palo baston",
+    "value": 6,
+    "china": 0
+  },
+  {
+    "name": "Moena",
+    "value": 5,
+    "china": 0
+  },
+  {
+    "name": "Almendro",
+    "value": 5,
+    "china": 0
+  },
+  {
+    "name": "Chancaquero",
+    "value": 4,
+    "china": 0
+  },
+  {
+    "name": "Caimitillo",
+    "value": 3,
+    "china": 1
+  },
+  {
+    "name": "Nogal amarillo",
+    "value": 3,
+    "china": 0
+  },
+  {
+    "name": "Couma macrocarpa",
+    "value": 3,
+    "china": 0
+  },
+  {
+    "name": "Tulpay",
+    "value": 3,
+    "china": 0
+  },
+  {
+    "name": "Carapa",
+    "value": 3,
+    "china": 0
+  },
+  {
+    "name": "Dacryodes olivifera",
+    "value": 2,
+    "china": 0
+  },
+  {
+    "name": "Capinuri",
+    "value": 2,
+    "china": 2
+  },
+  {
+    "name": "Brosimum alicastrum",
+    "value": 2,
+    "china": 0
+  },
+  {
+    "name": "Paramachaerium ormosioide",
+    "value": 2,
+    "china": 0
+  },
+  {
+    "name": "Brosimum sp.",
+    "value": 2,
+    "china": 0
+  },
+  {
+    "name": "Manchinga",
+    "value": 2,
+    "china": 0
+  },
+  {
+    "name": "Clarisia racemosa",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Yacushapana",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Madera comercial, largo-angosta",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Cepanchina",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Hura crepitans",
+    "value": 1,
+    "china": 1
+  },
+  {
+    "name": "Dyplotropis sp",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Aguano mashaMadera corta",
+    "value": 1,
+    "china": 1
+  },
+  {
+    "name": "Terminalia oblonga",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Copaifera reticulata",
+    "value": 1,
+    "china": 0
+  },
+  {
+    "name": "Paramachaerium sp.",
+    "value": 1,
+    "china": 0
+  }
+];
